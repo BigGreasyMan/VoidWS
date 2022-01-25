488e99b5bd076ada806eb87582301402abaa5148
Paul <paulvitty6@gmail.com>
2022-01-25 17:00:40 +0000

    IndexPage Update

--- TheVoidOfficial2/wwwroot/js/Helper.js
+++ TheVoidOfficial2/wwwroot/js/Helper.js
@@ -3,11 +3,32 @@
 function initAppendSpace()
 {
     textComp = document.getElementById("textComp");
-    console.log(textComp)
+    //Econsole.log(textComp)
 }
 function appendSpace(text)
 {
+    //TODO optimise
     var textComp = document.getElementById("textComp");
     textComp.innerHTML += text;
-    console.log(this.textComp.innerHTML)
+    //console.log(this.textComp.innerHTML)
 }
+function animationFade(name)
+{
+   console.log("Fade Started")
+    var element = document.getElementById(name);
+
+    let anim1 = setInterval(function () {
+        var final = null;
+       
+        element.style.opacity = parseFloat(element.style.opacity) + 0.02;
+
+        if (parseFloat(element.style.opacity) >= 1) {
+            clearInterval(anim1)
+            console.log("Fade In Finished");
+        }
+
+    }, 20)
+}
+
+
+
